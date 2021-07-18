9c8c92c35161bfd7f40debc824f763dadbfb6afb
Reem AlRashed <reemalrashed2@gmail.com>
2021-07-18 18:09:39 +0300

    Added interfaces

---
+++ src/interfaces/Order.ts
@@ -0,0 +1,13 @@
+export interface OrderType {
+    product_id: number;
+    quantity: number;
+    user_id: number;
+    status: string;
+  }
+  export interface OrderReturnType {
+    id: number;
+    product_id: number;
+    quantity: number;
+    user_id: number;
+    status: string;
+  }
---
+++ src/interfaces/Product.ts
@@ -0,0 +1,12 @@
+export interface ProductType {
+    name: string;
+    price: string;
+    category: string;
+  }
+  export interface ProductReturnType {
+    id: number;
+    name: string;
+    price: string;
+    category: string;
+  }
+  
